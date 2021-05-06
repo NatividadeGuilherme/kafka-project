ce1f455ab3bfe1880335b09dfb26e88872ceba14
NatividadeGuilherme <guilhermenatividade450@hotmail.com>
2021-05-05 21:59:29 -0300

    Inclusão projeto

---
+++ src/main/java/br/natividade/usingkafka/UsingKafkaApplication.java
@@ -0,0 +1,13 @@
+package br.natividade.usingkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/natividade/usingkafka/UsingKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.natividade.usingkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
